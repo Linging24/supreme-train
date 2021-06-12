27dca6f47dd6df277c1942507d83701c04d830be
Linging <1359635408@qq.com>
2021-06-12 11:36:00 +0800

    master主分支开发代码222

---
+++ src/test/java/top/linging/demo01submitform/Test2.java
@@ -0,0 +1,13 @@
+package top.linging.demo01submitform;
+
+/**
+ * @author Linging
+ * @version 1.0.0
+ * @since 1.0
+ */
+public class Test2 {
+
+    public static void main(String[] args) {
+        System.out.println("主分支代码");
+    }
+}
