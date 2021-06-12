89743bb4a2e7d109b6483139a214128a10669936
Linging <1359635408@qq.com>
2021-06-12 11:44:58 +0800

    dev主分支开发代码222

---
+++ src/test/java/top/linging/demo01submitform/Test3.java
@@ -0,0 +1,12 @@
+package top.linging.demo01submitform;
+
+/**
+ * @author Linging
+ * @version 1.0.0
+ * @since 1.0
+ */
+public class Test3 {
+    public static void main(String[] args) {
+        System.out.println("222222dev");
+    }
+}
---
+++ src/test/java/top/linging/demo01submitform/Test4.java
@@ -0,0 +1,10 @@
+package top.linging.demo01submitform;
+
+public class Test4 {
+    public static void main(String[] args) {
+        System.out.println("222222dev");
+    }
+    public void test(){
+        System.out.println("22222");
+    }
+}
